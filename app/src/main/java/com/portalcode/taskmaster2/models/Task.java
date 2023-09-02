f57549b04d39c4a71aec79c5b9d1b6fc97ae45e9
David Jon Oswald <doswald27@gmail.com>
2023-09-02 00:21:17 -0400

    state, task and TM database edits

--- app/src/main/java/com/portalcode/taskmaster2/models/Task.java
+++ app/src/main/java/com/portalcode/taskmaster2/models/Task.java
@@ -3,29 +3,32 @@ package com.portalcode.taskmaster2.models;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Date;
+
 @Entity // Annotation to indicate that this is an entity for Room
 public class Task {
 
     @PrimaryKey(autoGenerate = true) // Annotating a field as the primary key
-    private int id;
-
-    private String name;
-    private String body;
-    private State state;
+    public Long id;
+    String name;
+    String body;
+    java.util.Date dateCreated;
+    State state;
 
     public Task(String name, String body, State state) {
         this.name = name;
         this.body = body;
+        this.dateCreated = dateCreated;
         this.state = state;
     }
 
     // Getter and setter methods for fields (name, body, state)
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -51,5 +54,13 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
     }
 }
